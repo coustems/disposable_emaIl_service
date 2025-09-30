fac0bf7b0e5844948e19838de6614b84493d3fbc
coustems <rathoresumit10a@gmail.com>
2025-09-30 19:04:53 +0530

    Made changes

--- frontend/src/pages/NavBar.tsx
+++ frontend/src/pages/NavBar.tsx
@@ -1,15 +1,26 @@
-
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
-  const navigate = useNavigate();
   return (
     <nav className="navbar">
       <div className="navbar-links">
-  <NavLink to="/" className={({isActive}) => "navbar-link" + (isActive ? " active" : "")}>Home</NavLink>
-  <NavLink to="/about" className={({isActive}) => "navbar-link" + (isActive ? " active" : "")}>About</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            "navbar-link" + (isActive ? " active" : "")
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            "navbar-link" + (isActive ? " active" : "")
+          }
+        >
+          About
+        </NavLink>
       </div>
     </nav>
   );
